e70e2660ecfbfe3d39343f7d581281f4c707a0f1
VirussInside <artiom.vallat@gmail.com>
2020-10-26 22:11:22 +0100

    Added Player class to create multiple players and set their name, score and player number.

---
+++ memoryM4WinForm/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memoryM4WinForm
+{
+    public class Player
+    {
+        public int playerScore { get; set; }
+        public string playerName { get; set; }
+        public int playerNumber { get; set; }
+
+    }
+}
